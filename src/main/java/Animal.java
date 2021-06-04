d1d4ae31568f8789b68ea7c209a148b51996ff81
Junjie Li <790225650@qq.com>
2021-06-03 23:53:18 -0400

    first commit

---
+++ src/main/java/Animal.java
@@ -0,0 +1,28 @@
+public class Animal {
+    public void animalSound() {
+        System.out.println("The animal makes a sound");
+    }
+    public static void main(String[] args){
+        while(true){
+
+        }
+    }
+}
+
+class Pig extends Animal {
+    public void animalSound() {
+        try{
+            System.out.println("The pig says: wee wee");
+        }
+        catch (Exception e){
+
+        }
+
+    }
+}
+
+class Dog extends Animal {
+    public void animalSound() {
+        System.out.println("The dog says: bow wow");
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
-public class DDAnomaly {
+public class Main {
     public static void testDD(){
         int count= 1+1+
-        1;
+                1;
         count = count + 1;
         count = count/1;
         count = count*1;
         count = count - 1;
         count++;
         count++;
+
     }
     public static void main(String[] args){
 
